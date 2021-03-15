85d31bf61fcd65d6524bda134ddcc8de221770de
Gabriel <gabriel.jchen@gmail.com>
2021-03-15 22:14:11 +0000

    added random string generator with help from stackoverflow

--- express_server.js
+++ express_server.js
@@ -1,7 +1,11 @@
 const express = require('express');
 const app = express();
 const PORT = 8080;
+const bodyParser = require('body-parser')
+app.use(bodyParser.urlencoded({extended: true}));
 
+
+app.set("view engine", "ejs");
 
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
@@ -12,6 +16,7 @@ app.get('/', (req, res) => {
   res.send("Hello!");
 })
 
+
 app.get('/urls.json', (req, res) => {
   res.json(urlDatabase);
 })
@@ -20,6 +25,41 @@ app.get('/hello', (req, res) => {
   res.send("<html><body>Hello <b> World</b></body></html>\n");
 })
 
+app.get('/urls', (req, res) => {
+  const templateVars = {urls:urlDatabase};
+  res.render('urls_index', templateVars)
+})
+
+app.get('/urls/new', (req, res) => {
+  res.render('urls_new')
+})
+
+app.get("/urls/:shortURL", (req, res) => {
+  const templateVars = { shortURL: req.params.shortURL, longURL: req.params.longURL };
+  res.render("urls_show", templateVars)
+})
+
+app.post('/urls', (req, res) => {
+  console.log(req.body);
+  res.send('OK');
+})
+
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}`);
 })
+
+
+function generateRandomString() {
+
+  let alphaNum = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+
+  let randomString = '';
+
+  for (let i = 0; i < 6; i++) {
+    
+    randomString += alphaNum.charAt(Math.floor(Math.random() * alphaNum.length));
+    
+  }
+  
+  return randomString;
+}
